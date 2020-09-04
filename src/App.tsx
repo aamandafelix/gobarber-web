d570a1518111567a15c772d8012e81567898a01a
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-09-04 19:49:03 -0300

    Setup global style with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => {
-  return <h1>Hello workd</h1>;
+  return (
+    <>
+      <h1>GoBarber</h1>
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
