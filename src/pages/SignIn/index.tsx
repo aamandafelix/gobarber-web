fc371c8e746f9f2b244d7021cd2af62ce8a8fd92
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-09-05 15:57:51 -0300

    Create signin page structure

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <Input name="email" icon={FiMail} placeholder="Email" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </form>
+
+        <a href="register">
+          <FiLogIn size={20} />
+          Criar conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default SignIn;
