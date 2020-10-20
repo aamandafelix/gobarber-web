176436208a8981870a16916b69d7ad226d7cc86b
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-10-20 13:30:22 -0300

    Make icon optional in input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -14,7 +14,7 @@ import { Container, Error } from './styles';
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   containerStyle?: Record<string, unknown>;
-  icon: React.ComponentType<IconBaseProps>;
+  icon?: React.ComponentType<IconBaseProps>;
 }
 
 const Input: React.FC<InputProps> = ({
