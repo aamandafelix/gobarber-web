f177f42ed0fd9a7085917136cf2959759b4deafe
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-10-21 20:22:59 -0300

    Add tests for input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -53,6 +53,7 @@ const Input: React.FC<InputProps> = ({
       isErrored={!!error}
       isFocused={isFocused}
       isFilled={isFilled}
+      data-testid="input-container"
     >
       {Icon && <Icon size={20} />}
       <input
